128a66f5e5c671cef0d7cd694704508b29ff039d
Anas <nafeaanass15@gmail.com>
2022-05-28 16:56:07 +0100

    Plugin Done

---
+++ Contact_Form.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name:  Contact Form Plugin
+Description: Simple WordPress Contact Form
+Author: Anas Nafea
+*/
+
+function html_form_code() {
+	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
+	echo '<p>';
+	echo 'Your Name (required) <br/>';
+	echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" style="width:100%" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Email (required) <br/>';
+	echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" style="width:100%" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Subject (required) <br/>';
+	echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" style="width:100%" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Message (required) <br/>';
+	echo '<textarea rows="3" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
+	echo '</p>';
+	echo '<p><input type="submit" name="cf-submitted" value="Send"></p>';
+	echo '</form>';
+}
+
+function deliver_mail() {
+
+	// if the submit button is clicked, send the email
+	if ( isset( $_POST['cf-submitted'] ) ) {
+
+		// sanitize form values
+		$name    = sanitize_text_field( $_POST["cf-name"] );
+		$email   = sanitize_email( $_POST["cf-email"] );
+		$subject = sanitize_text_field( $_POST["cf-subject"] );
+		$message = esc_textarea( $_POST["cf-message"] );
+
+		// get the blog administrator's email address
+		$to = get_option( 'admin_email' );
+
+		$headers = "From: $name <$email>" . "\r\n";
+
+		// If email has been process for sending, display a success message
+		if ( wp_mail( $to, $subject, $message, $headers ) ) {
+			echo '<div>';
+			echo '<p>Thanks for contacting me, expect a response soon.</p>';
+			echo '</div>';
+		} else {
+			echo 'An unexpected error occurred';
+		}
+	}
+}
+
+function cf_shortcode() {
+	ob_start();
+	deliver_mail();
+	html_form_code();
+
+	return ob_get_clean();
+}
+
+add_shortcode( 'sitepoint_contact_form', 'cf_shortcode' );
+
